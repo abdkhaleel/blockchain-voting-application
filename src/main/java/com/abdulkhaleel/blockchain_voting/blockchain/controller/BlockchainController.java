93cd4ee9b4a53d38f9e1e3fe680571ad1bc9cc60
abdkhaleel <abdkhaleel16@gmail.com>
2025-06-20 19:42:49 +0530

    controller for results inspection

---
+++ src/main/java/com/abdulkhaleel/blockchain_voting/blockchain/controller/BlockchainController.java
@@ -0,0 +1,26 @@
+package com.abdulkhaleel.blockchain_voting.blockchain.controller;
+
+import com.abdulkhaleel.blockchain_voting.blockchain.model.Block;
+import com.abdulkhaleel.blockchain_voting.blockchain.service.BlockchainService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/blockchain")
+@RequiredArgsConstructor
+public class BlockchainController {
+
+    private final BlockchainService blockchainService;
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<Block>> getFullChain() {
+        return ResponseEntity.ok(blockchainService.getChain());
+    }
+}
